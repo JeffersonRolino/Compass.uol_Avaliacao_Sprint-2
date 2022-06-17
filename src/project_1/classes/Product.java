90fae13a6c64f66ea81ee5f8425e8b047885aea2
JeffersonRolino <jefferson.rolino@gmail.com>
2022-06-17 15:15:52 -0300

    Criada a classe Product

---
+++ src/project_1/classes/Product.java
@@ -0,0 +1,57 @@
+package project_1.classes;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private int quantity;
+    private double price;
+
+    public Product(String _name, String _description, int _quantity, double _price){
+        this.name = _name;
+        this.description = _description;
+        this.quantity = _quantity;
+        this.price = _price;
+    }
+
+    // Getters and Setters...
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
