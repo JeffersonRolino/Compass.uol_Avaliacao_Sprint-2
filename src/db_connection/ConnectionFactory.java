c6faab600841cb2fb65a01c9db68c82a20f8ffad
JeffersonRolino <jefferson.rolino@gmail.com>
2022-06-17 15:03:06 -0300

    Criado simples Connection Factory no package db_connection para usar em todos os projetos

---
+++ src/db_connection/ConnectionFactory.java
@@ -0,0 +1,18 @@
+package db_connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public Connection getConnection(String _dataBase){
+        String ip = "jdbc:mysql://localhost:3306/";
+        String url = ip + _dataBase;
+        try {
+            return DriverManager.getConnection(url, "root", "root");
+        } catch (SQLException exception){
+            throw new RuntimeException(exception);
+        }
+    }
+}
