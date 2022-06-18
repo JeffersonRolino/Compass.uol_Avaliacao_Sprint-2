0956b5a099a5d04a3777cf055b8ee67b97c680a1
JeffersonRolino <jefferson.rolino@gmail.com>
2022-06-18 01:30:35 -0300

    Adicionado método select e selectAll em ProductDAO

--- src/project_1/daos/ProductDAO.java
+++ src/project_1/daos/ProductDAO.java
@@ -4,10 +4,51 @@ import db_connection.ConnectionFactory;
 import project_1.classes.Product;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ProductDAO {
+
+    public static void selectAll(){
+        Connection connection = ConnectionFactory.getConnection("products_db");
+        String sqlCommand = "select `id`, `name`, `description`, `quantity`, `price` from `products_db`.`product`;";
+
+        try(Statement stm = connection.createStatement()){
+            ResultSet resultSet = stm.executeQuery(sqlCommand);
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                int quantity = resultSet.getInt("quantity");
+                double price = resultSet.getDouble("price");
+
+                System.out.println("Produto: " + id + ", " + name + ", " + description + ", " + quantity + ", R$ " + price);
+            }
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public static void select(int _id){
+        Connection connection = ConnectionFactory.getConnection("products_db");
+        String sqlCommand = "select `id`, `name`, `description`, `quantity`, `price` from `products_db`.`product` where `id` = '" + _id + "';";
+
+        try(Statement stm = connection.createStatement()){
+            ResultSet resultSet = stm.executeQuery(sqlCommand);
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                int quantity = resultSet.getInt("quantity");
+                double price = resultSet.getDouble("price");
+
+                System.out.println("Produto: " + id + ", " + name + ", " + description + ", " + quantity + ", R$ " + price);
+            }
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
+    }
 
     public static void save(Product product){
         Connection connection = ConnectionFactory.getConnection("products_db");
---
+++ src/project_1/tests/SelectTest.java
@@ -0,0 +1,9 @@
+package project_1.tests;
+
+import project_1.daos.ProductDAO;
+
+public class SelectTest {
+    public static void main(String[] args) {
+        ProductDAO.selectAll();
+    }
+}
